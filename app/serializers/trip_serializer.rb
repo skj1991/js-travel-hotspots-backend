4b0eddb29fd5d54149cc1cc17a89785e964cb7bf
skj1991 <stacykjohns@icloud.com>
2020-09-26 17:47:41 -0400

    added CORS gem and uncommented code in cors.rb file. added country_id to trip_serializer.

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,4 @@
 class TripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :city, :description, :rating, :hotel, :must_visit, :top_restaurant, :image_url, :country
+  attributes :title, :city, :description, :rating, :hotel, :must_visit, :top_restaurant, :image_url, :country_id, :country
 end
